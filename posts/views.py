de8120ea043fb7e1a36af9923abe34f6bb723fab
trevin-livele <liveletrevin6@gmail.com>
2022-04-03 13:27:34 +0300

    finished end points

--- posts/views.py
+++ posts/views.py
@@ -9,10 +9,26 @@ from rest_framework.response import Response
 class PostList(generics.ListCreateAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
-    permission_classes =[permissions.IsAuthenticated]
+    permission_classes =[permissions.IsAuthenticatedOrReadOnly]
 
     def perform_create(self, serializer):
         serializer.save(poster=self.request.user)
+
+
+
+
+
+class PostRetrieveDestroy(generics.RetrieveDestroyAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes =[permissions.IsAuthenticatedOrReadOnly]
+
+    def delete(self, request, *args, **kwargs):
+        post = Post.objects.filter(pk=kwargs['pk'],poster=self.request.user)
+        if post.exists():
+            return self.destroy(request, *args, **kwargs)
+        else:
+            raise ValidationError('This isn\'t your post to delete BRUH!!!')
 
 class VoteCreate(generics.CreateAPIView,mixins.DestroyModelMixin):
     serializer_class = VoteSerializer
