741c01e27bd1bb50f2d2ad80188c7d33fa7319d3
trevin-livele <liveletrevin6@gmail.com>
2022-04-03 12:09:00 +0300

    added function to display the votes

--- posts/views.py
+++ posts/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
-from rest_framework import generics,permissions
+from rest_framework import generics,permissions,mixins,status
 from rest_framework.exceptions import ValidationError
 from .models import Post,Vote
 from .serializers import PostSerializer, VoteSerializer
+from rest_framework.response import Response
 
 
 class PostList(generics.ListCreateAPIView):
@@ -13,7 +14,7 @@ class PostList(generics.ListCreateAPIView):
     def perform_create(self, serializer):
         serializer.save(poster=self.request.user)
 
-class VoteCreate(generics.CreateAPIView):
+class VoteCreate(generics.CreateAPIView,mixins.DestroyModelMixin):
     serializer_class = VoteSerializer
     permission_classes =[permissions.IsAuthenticated]
 
@@ -26,5 +27,12 @@ class VoteCreate(generics.CreateAPIView):
     def perform_create(self, serializer):
         if self.get_queryset().exists():
             raise ValidationError('you have already voted for this post')
-        serializer.save(voter=self.request.user,post = Post.objects.get(pk=self.kwargs['pk'])
-)
+        serializer.save(voter=self.request.user,post = Post.objects.get(pk=self.kwargs['pk']))
+
+
+    def delete(self, request, *args, **kwargs):
+        if self.get_queryset().exists():
+            self.get_queryset().delete() 
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        else:
+            raise ValidationError('You never voted for this post.....silly!!')
